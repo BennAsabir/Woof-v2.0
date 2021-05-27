e34b081c8ca24ef76da49c5cd84f8d0cfd41478f
Benzino <benasabir@gmail.com>
2021-05-27 13:56:43 -0400

    transferred all files

---
+++ client/src/components/About/AboutContact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function AboutContact() {
+    return (
+        <>
+            <div className="about-contact-banner">
+                    <span className="about-caption">Contact Us</span>
+            </div>
+        </>
+    )
+}
+
+export default AboutContact;
