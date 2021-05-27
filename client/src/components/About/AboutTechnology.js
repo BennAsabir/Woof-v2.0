42bf26597239cd4729e9a44ee2ab7df3f229f30c
Benzino <benasabir@gmail.com>
2021-05-27 14:19:34 -0400

    image changes

--- client/src/components/About/AboutTechnology.js
+++ client/src/components/About/AboutTechnology.js
@@ -6,12 +6,12 @@ function AboutTechnology() {
         <>
         <div className="flex-tech">
             <div className="tech-card">
-                <img className="tech-picture" alt="gps" src={process.env.PUBLIC_URL + '/images/about-gps.jpeg'} />
+                <img className="tech-picture" alt="gps" src={process.env.PUBLIC_URL + '/images/location.jpeg'} />
                 <p><b>Our commitment is your safety</b></p>
                 <p>With every safety feature and every standard in our Community Guidelines, we're committed to helping to create a safe environment for our users.</p>
             </div>
             <div className="tech-card">
-                <img className="tech-picture" alt="gps" src={process.env.PUBLIC_URL + '/images/about-payment.png'} />
+                <img className="tech-picture" alt="gps" src={process.env.PUBLIC_URL + '/images/payment.jpeg'} />
                 <p><b>Easy payment options</b></p>
                 <p>Our app is available with the best payment options, which provides secure, easy and fast payments for our users.</p>
             </div>
